a1282d5795c926af57d1e0201c6e662255baa00c
httpE2Barao <elias.barao@hotmail.com>
2025-03-01 13:15:10 -0300

    Update shows.ts

--- src/data/shows.ts
+++ src/data/shows.ts
@@ -2,38 +2,38 @@ import { Show } from '../components/ShowCard';
 
 export const shows: Show[] = [
   {
-    date: "3 Abril",
-    venue: "Czar",
+    date: "3 Março",
+    venue: "Carna Czar",
     city: "Curitiba, PR",
     time: "23:15",
-    ticketUrl: "https://example.com/tickets/1"
+    ticketUrl: "https://meaple.com.br/czarcuritiba/carnaczar-0303-segunda-de-carnaval"
   },
   {
-    date: "4 Abril",
+    date: "4 Março",
     venue: "Risoflora",
     city: "Curitiba, PR",
     time: "16:30",
-    ticketUrl: "https://example.com/tickets/2"
+    ticketUrl: "https://ticketdigitall.com.br/agenda/risoflora-2025"
   },
   {
-    date: "7 Abril",
+    date: "7 Março",
     venue: "O tal do Quintal",
     city: "Curitiba, PR",
     time: "21:30",
     ticketUrl: "https://example.com/tickets/3"
   },
   {
-    date: "8 Abril",
+    date: "8 Março",
     venue: "Makunfest",
     city: "Curitiba, PR",
-    time: "indisponível",
-    ticketUrl: "https://example.com/tickets/3"
+    time: "17:00",
+    ticketUrl: "https://www.diskingressos.com.br/event/8559?fbclid=PAZXh0bgNhZW0CMTEAAaa7V_jpFQZTjb-zutZqbNrOuj2joptmT83YeL3WLLVF08Y5JN8v2y050Yg_aem_26QuHUr2ZFjupdhukDhQig"
   },
   {
-    date: "9 Abril",
+    date: "9 Março",
     venue: "Samba da Nêga feat. DJ Will",
     city: "São Paulo, SP",
     time: "17:00",
-    ticketUrl: "https://example.com/tickets/3"
+    ticketUrl: "https://www.sympla.com.br/evento/samba-da-nega/2835457?fbclid=PAZXh0bgNhZW0CMTEAAabAoa4qbfHyPz2i-3iPkYUEC5vYCOAgIevRZAjbsuce_GXtkFKZf8sPgKg_aem_VbXl5GX-tCE2wRa9L0GYaQ&referrer=l.instagram.com"
   },
 ];
