9c7138b84db8b881f901f0a4949cefdd53f32f08
Elias E. Barão <e2barao@hotmail.com>
2025-03-10 14:38:59 -0300

    Update shows.ts

--- src/data/shows.ts
+++ src/data/shows.ts
@@ -1,31 +1,32 @@
-import { Show } from '../components/ShowCard';
-
-export const shows: Show[] = [
   {
     date: "21 Março",
-    venue: "Oficina Nossos Tambores não se calam",
+    venue: "🥁 Oficina Nossos Tambores não se calam",
     city: "Conservatório de Mpb de Curitiba",
     time: "17h às 21h",
     ticketUrl: "https://forms.gle/WWQ1Vyj1eQrikPyt7",
     ehOficina: true,
   },{
     date: "22 Março",
-    venue: "Oficina Compondo em oralidades",
+    venue: "✍🏼 Oficina Compondo em oralidades",
     city: "Conservatório de Mpb de Curitiba",
     time: "9h às 12h",
     ticketUrl: "https://forms.gle/Dg4V36YsdAxvgH7M9",
     ehOficina: true,
   },{
     date: "22 Março",
-    venue: "Bate Papo - A história do Samba e a cena artística de Curitiba.",
+    venue: "📢 Bate Papo - A história do Samba e a cena artística de Curitiba.",
     city: "Conservatório de Mpb de Curitiba",
     time: "10h às 12h"
   },{
     date: "22, 23 Março",
-    venue: "Oficina de Dança - Método MP",
+    venue: "💃🏿🕺🏿 Oficina de Dança - Método MP",
     city: "Casa Hoffmann",
     time: "Dia 22: 18h às 22h; Dia 23: 15h às 19h",
     ticketUrl: "https://forms.gle/KDHTN9NMccQ9dwTm6",
     ehOficina: true,
+  },{
+    date: "22 Março",
+    venue: "🥁🎤 Roda de Samba-Bloco Boca Negra Convida O Samba da Nêga Tecnologia Ancestral",
+    city: "Casa Hoffmann",
+    time: "15h as 17h",
   },
-];
