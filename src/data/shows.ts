516724388d639b8a763ddac725eda45166db93ec
Elias E. Barão <e2barao@hotmail.com>
2025-03-10 14:22:39 -0300

    Update shows.ts

--- src/data/shows.ts
+++ src/data/shows.ts
@@ -2,37 +2,29 @@ import { Show } from '../components/ShowCard';
 
 export const shows: Show[] = [
   {
-    date: "3 Março",
-    venue: "Carna Czar",
-    city: "Curitiba, PR",
-    time: "23:15",
-    ticketUrl: "https://meaple.com.br/czarcuritiba/carnaczar-0303-segunda-de-carnaval"
-  },
-  {
-    date: "4 Março",
-    venue: "Risoflora",
-    city: "Curitiba, PR",
-    time: "16:30",
-    ticketUrl: "https://ticketdigitall.com.br/agenda/risoflora-2025"
-  },
-  {
-    date: "7 Março",
-    venue: "O tal do Quintal",
-    city: "Curitiba, PR",
-    time: "21:30",
-  },
-  {
-    date: "8 Março",
-    venue: "Makunfest",
-    city: "Curitiba, PR",
-    time: "17:00",
-    ticketUrl: "https://www.diskingressos.com.br/event/8559?fbclid=PAZXh0bgNhZW0CMTEAAaa7V_jpFQZTjb-zutZqbNrOuj2joptmT83YeL3WLLVF08Y5JN8v2y050Yg_aem_26QuHUr2ZFjupdhukDhQig"
-  },
-  {
-    date: "9 Março",
-    venue: "Samba da Nêga feat. DJ Will",
-    city: "São Paulo, SP",
-    time: "17:00",
-    ticketUrl: "https://www.sympla.com.br/evento/samba-da-nega/2835457?fbclid=PAZXh0bgNhZW0CMTEAAabAoa4qbfHyPz2i-3iPkYUEC5vYCOAgIevRZAjbsuce_GXtkFKZf8sPgKg_aem_VbXl5GX-tCE2wRa9L0GYaQ&referrer=l.instagram.com"
+    date: "21 Março",
+    venue: "Oficina Nossos Tambores não se calam",
+    city: "Conservatório de Mpb de Curitiba",
+    time: "17h às 21h",
+    ticketUrl: "https://forms.gle/WWQ1Vyj1eQrikPyt7"
+  },{
+    date: "22 Março",
+    venue: "Oficina Compondo em oralidades",
+    city: "Conservatório de Mpb de Curitiba",
+    time: "9h às 12h",
+    ticketUrl: "https://forms.gle/Dg4V36YsdAxvgH7M9"
+  },{
+    date: "22 Março",
+    venue: "Bate Papo - A história do Samba e a cena artística de Curitiba.",
+    city: "Conservatório de Mpb de Curitiba",
+    time: "10h às 12h",
+
+  },{
+    date: "22, 23 Março",
+    venue: "Oficina de Dança - Método MP",
+    city: "Casa Hoffmann",
+    time: "Dia 22: 18h às 22h; Dia 23: 15h às 19h",
+    ticketUrl: " https://forms.gle/KDHTN9NMccQ9dwTm6 
+"
   },
 ];
