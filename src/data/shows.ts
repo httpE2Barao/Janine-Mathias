b61d86b0bbf27d149fdeb171a98632513545b70c
Elias E. Barão <e2barao@hotmail.com>
2025-03-10 14:25:24 -0300

    Update shows.ts

--- src/data/shows.ts
+++ src/data/shows.ts
@@ -17,14 +17,12 @@ export const shows: Show[] = [
     date: "22 Março",
     venue: "Bate Papo - A história do Samba e a cena artística de Curitiba.",
     city: "Conservatório de Mpb de Curitiba",
-    time: "10h às 12h",
-
+    time: "10h às 12h"
   },{
     date: "22, 23 Março",
     venue: "Oficina de Dança - Método MP",
     city: "Casa Hoffmann",
     time: "Dia 22: 18h às 22h; Dia 23: 15h às 19h",
-    ticketUrl: " https://forms.gle/KDHTN9NMccQ9dwTm6 
-"
+    ticketUrl: "https://forms.gle/KDHTN9NMccQ9dwTm6"
   },
 ];
